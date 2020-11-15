67a8b4103a6f84ebc01b64102143c2b918731e09
berezka-creator <berezina.y.v.2015@gmail.com>
2020-11-15 14:47:13 -0800

    Updates

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import CourseContainer from "./components/CourseContainer"
-
-
-
-ReactDOM.render(<CourseContainer />, document.getElementById("root"))
